be2a75471d6cbccc73b0d91bb18ddc692a3e1a22
Rayon Nunes <rayonnunes@hotmail.com>
2019-12-01 01:43:28 -0300

    instalação do vue através do vue/cli

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="nav-bar"></div>
+    <div id="app">
+      <shopping-cart/>
+    </div>
+  </div>
+</template>
+
+<script>
+require ('@/assets/css/master.css')
+import ShoppingCart from './components/ShoppingCart'
+
+export default {
+  name: 'app',
+  components: {
+    'shopping-cart': ShoppingCart
+  }
+}
+</script>
